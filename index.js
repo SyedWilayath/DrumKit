ac3c027f24bbdc3a9c46b64442e941592aacbb6c
Syed Wilayath Khadari <syedwilayath2@gmail.com>
2022-02-01 14:34:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,45 +17,68 @@ for (var i = 0; i < noOfDrums; i++) {
 
     console.log(this.innerHTML);
     var keyPressInnerHTML = this.innerHTML;
-
-    switch (keyPressInnerHTML) {
-      case "w":
-        var audio = new Audio("sounds/crash.mp3");
-        audio.play();
-        break;
-
-      case "a":
-        var audio = new Audio("sounds/kick-bass.mp3");
-        audio.play();
-        break;
-
-      case "s":
-        var audio = new Audio("sounds/tom-1.mp3");
-        audio.play();
-        break;
-
-      case "d":
-        var audio = new Audio("sounds/tom-2.mp3");
-        audio.play();
-        break;
-
-      case "j":
-        var audio = new Audio("sounds/tom-3.mp3");
-        audio.play();
-        break;
-
-      case "k":
-        var audio = new Audio("sounds/tom-4.mp3");
-        audio.play();
-        break;
-
-      case "l":
-        var audio = new Audio("sounds/snare.mp3");
-        audio.play();
-        break;
-    }
+    produceSound(keyPressInnerHTML);
+    buttonAnimation(keyPressInnerHTML);
   });
 }
 
-// var audio = new Audio("sounds/tom-1.mp3");
-// audio.play();
+document.addEventListener('keydown', function(keyPress) {
+  console.log(keyPress.key);
+
+  var keyPressed = keyPress.key;
+
+  produceSound(keyPressed)
+  buttonAnimation(keyPressed);
+
+});
+
+
+function produceSound(trigger) {
+
+  switch (trigger) {
+    case "w":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+
+    case "a":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+
+    case "s":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+
+    case "d":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+
+    case "j":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+
+    case "k":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+
+    case "l":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+  }
+
+}
+
+function buttonAnimation(currentKey){
+  var activeButton = document.querySelector("."+currentKey);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  },100);
+}
